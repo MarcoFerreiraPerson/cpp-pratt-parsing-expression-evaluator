fb2900430e1ee4a621f9918aecaa5535636ae408
Marco Ferreira <marcoferreiraperson@gmail.com>
2025-11-14 23:42:17 -0500

    updated gitignore:

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,14 @@ int main(int argc, char *argv[])
 {
     std::string test_expression = "1 + 1 * 5 + 2";
 
+    /*
+    a = 5
+    b = 6
+    c = b + 1
+
+    1 + c + 3 * a
+    
+    */
+
     return 0;
 }
